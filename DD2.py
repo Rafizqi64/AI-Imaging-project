4060403b54d5f14f4ad14315f84a0d3914f9f8c9
Rafizqi64 <75040796+Rafizqi64@users.noreply.github.com>
2023-04-06 13:19:08 +0200

    Sucessfully comitted the latest version

--- DD2.py
+++ DD2.py
@@ -61,10 +61,8 @@ def get_model(kernel_size=(3,3), pool_size=(2,2), first_filters=64, second_filte
     model.add(MaxPool2D(pool_size=pool_size))
     
     model.add(Flatten())
-    #model.add(Dense(2048, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
+    # model.add(Dense(1024, activation='relu'))
+    # model.add(Dropout(0.5))
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(256, activation='relu'))
